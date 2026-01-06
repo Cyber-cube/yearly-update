a1c8e6421ea9949df7bd82ee224cc81693f99d69
Cyber-cube <cyber.cube6198@gmail.com>
2026-01-06 19:46:18 +0530

    Updates

--- src/index.js
+++ src/index.js
@@ -9,6 +9,37 @@
  */
 
 export default {
+	async scheduled(controllee, env, ctx) {
+		const sendRequest = async (repoName) => {
+			await fetch(`https://api.github.com/repos/Cyber-cube/${repoName}/actions/workflows/yearly-update/`, {
+				headers: {
+					"Accept": "application/vnd.github+json",
+					"X-GitHub-Api-Version": "2022-11-28",
+					"User-Agent": "Yearly-Updater",
+					"Authorization": `Bearer ${env.GH_TOKEN}`,
+				}
+			})
+		}
+		const data = {
+			1: "13-5",
+			2: "1-0"
+		}
+
+		const dateAsList = [new Date().getUTCDay(), new Date().getUTCMonth()]
+		const date = dateAsList.join("-")
+
+		const keys = Object.keys(data)
+		const need = keys.filter(key => data[key] === date)
+
+		if (need.length === 0) {
+			return
+		}
+
+		need.forEach((repoName) => {
+			sendRequest(repoName)
+		})
+
+	},
 	async fetch(request, env, ctx) {
 		return new Response('Hello World!');
 	},
