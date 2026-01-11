cebe1ed8c44bcc9083dd10c61859618af00baa76
Cyber-cube <cyber.cube6198@gmail.com>
2026-01-11 16:07:04 +0530

    Meow

--- src/index.js
+++ src/index.js
@@ -45,38 +45,6 @@ export default {
 
 	},
 	async fetch(request, env, ctx) {
-		/* const sendRequest = async (repoName) => {
-			await fetch(`https://api.github.com/repos/Cyber-cube/${repoName}/actions/workflows/yearly-update/`, {
-				headers: {
-					"Accept": "application/vnd.github+json",
-					"X-GitHub-Api-Version": "2022-11-28",
-					"User-Agent": "Yearly-Updater",
-					"Authorization": `Bearer ${env.GH_TOKEN}`,
-				}
-			})
-		} */
-		const data = {
-			1: "13-5",
-			2: "0-11",
-			3: "30-4",
-			4: "12-3",
-			5: "14-5",
-			18: "21-5",
-		}
-
-		const dateAsList = [new Date().getUTCDay(), new Date().getUTCMonth()]
-		const date = dateAsList.join("-")
-
-		const keys = Object.keys(data)
-		const need = keys.filter(key => data[key] === date)
-
-		if (need.length === 0) {
-			return
-		}
-
-		need.forEach((repoName) => {
-			// sendRequest(repoName)
-			console.log("Meow")
-		})
+		return new Response("Hello World!")
 	},
 };
