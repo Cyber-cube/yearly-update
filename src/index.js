a3b4134fa546e59272530002806f76b33990d388
Cyber-cube <cyber.cube6198@gmail.com>
2026-04-06 12:32:43 +0530

    test

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ export default {
 
 		const keys = Object.keys(data)
 		const need = keys.filter(key => data[key] === date)
-		console.log(need)
+		console.log(need, typeof need, need.length)
 		// console.log("Meow", need, date)
 		/* if (need.length === 0) {
 			return
